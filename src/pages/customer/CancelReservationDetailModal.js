b99831cc6b7de6bfff808c8182cc123b6da51edd
Jeong-in <162551171+munginisfree@users.noreply.github.com>
2024-07-17 01:46:55 +0900

    add reservation cancel modal

--- src/pages/customer/CancelReservationDetailModal.js
+++ src/pages/customer/CancelReservationDetailModal.js
@@ -1,9 +1,9 @@
 import React from 'react';
-
-const CancelReservationDetailModal = () => {
+// 필요한 정보 : 가격, 픽업마감시간, 예약자 닉네임, 가게 이름
+const CancelReservationDetailModal = ({reservation}) => {
     return (
         <div>
-
+            예약 취소 모달
         </div>
     );
 };
