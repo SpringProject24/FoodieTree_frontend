f36dbf3d059e13f7d01bd368f50763e8dea73d70
hansolkim9 <masicer2@gmail.com>
2024-07-18 16:23:34 +0900

    # customer-mypage: 예약 내역

--- src/pages/customer/CustomerMyPage.js
+++ src/pages/customer/CustomerMyPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Profile from "../../components/customer/mypage/Profile";
 import styles from "./CustomerMyPage.module.scss";
+import CustomerReservationList from "../../components/customer/mypage/CustomerReservationList";
 
 const CustomerMyPage = () => {
   return (
@@ -9,6 +10,7 @@ const CustomerMyPage = () => {
               <div className={styles.container}>
                   <Profile />
                   <div className={styles.content}>
+                      <CustomerReservationList />
                   </div>
               </div>
           </div>
