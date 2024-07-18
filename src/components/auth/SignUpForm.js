a28cf2e5b81229fb7628166a99fbf4054e561d1e
Maybaba <sinyunjong@gmail.com>
2024-07-19 04:19:58 +0900

    sign-up first design

--- src/components/auth/SignUpForm.js
+++ src/components/auth/SignUpForm.js
@@ -2,26 +2,23 @@
 import React, { useState } from 'react';
 import _ from 'lodash';
 import styles from './SignUpForm.module.scss';
-import commonStyles from '../../common.module.scss';
 
-const SignUpForm = ({ userType, onSignUp, onResendEmail }) => {
+
+const SignUpForm = ({ userType, onSignUp, onResendEmail, onVerificationSent }) => {
   const [email, setEmail] = useState('');
   const [emailValid, setEmailValid] = useState(false);
   const [verificationSent, setVerificationSent] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
 
-  // const BASE_URL = window.location.origin;
 
   const checkEmailInput = (email) => {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegex.test(email);
   };
 
-  // 새로운 아이디 -> 중복검사 후 ok
   const checkDupId = async (email) => {
     try {
-      // const response = await fetch(`${BASE_URL}/store/check?type=account&keyword=${email}`);
-      // const result = await response.json();
+      //fetch
       const result = false; // 더미 값: 이메일이 유효하다고 가정
       return !result;
     } catch (error) {
@@ -32,16 +29,7 @@ const SignUpForm = ({ userType, onSignUp, onResendEmail }) => {
 
   const sendVerificationLinkForSignUp = async (email) => {
     try {
-      // const response = await fetch(`${restfulapi~}/email/sendVerificationLink`, {
-      //   method: 'POST',
-      //   headers: {
-      //     'Content-Type': 'application/json',
-      //   },
-      //   body: JSON.stringify({
-      //     email,
-      //     purpose: 'signup',
-      //   }),
-      // });
+      //fetch
       const response = { ok: true }; // 더미 값: 요청이 성공했다고 가정
       return response.ok;
     } catch (error) {
@@ -73,56 +61,70 @@ const SignUpForm = ({ userType, onSignUp, onResendEmail }) => {
     setIsLoading(false);
     if (result) {
       setVerificationSent(true);
+      onVerificationSent(); // 상태를 부모 컴포넌트에 알림
     } else {
       alert('잠시 후 다시 시도해주세요.');
     }
   };
 
+  const handleRetrySignUp = () => {
+    console.log('Before resetting state:');
+    console.log('verificationSent:', verificationSent);
+    console.log('email:', email);
+    console.log('emailValid:', emailValid);
+
+    setVerificationSent(false);
+    setEmail('');
+    setEmailValid(false);
+
+
+    console.log('After resetting state:');
+    console.log('verificationSent:', verificationSent);
+    console.log('email:', email);
+    console.log('emailValid:', emailValid);
+
+  };
+
   return (
-    <div className={styles['sign-up-form']}>
-      {/*<header>*/}
-      {/*  <div className={styles.container}>*/}
-      {/*    <div className={`${styles.logo} ${styles['margarine-regular']}`}>FoodieTree</div>*/}
-      {/*    <div className={styles['logo-img']}>*/}
-      {/*      <img src="/assets/img/img_2.png" alt="Logo" />*/}
-      {/*    </div>*/}
-      {/*  </div>*/}
-      {/*</header>*/}
-      <section className={styles['input-area']}>
-        <form onSubmit={handleSendVerificationLink}>
-          <div className={styles.container}>
-            {verificationSent ? (
-                <div className={styles['verify-link-sent']}>
-                  <h2>{userType} 회원 등록을 위한 인증 링크가 이메일로 발송되었습니다.</h2>
-                  <p> [ {email} ] </p>
-                  <p>이메일을 확인하여 인증을 완료해주세요.</p>
-                  <button className={styles['resend-login-email-btn']} onClick={onResendEmail}>
-                    이메일을 받지 못하셨나요? 재전송하기
-                  </button>
-                </div>
-            ) : (
-                <div className={styles['id-wrapper']}>
-                  <h2>{userType} 회원 등록을 위한 이메일을 입력해주세요!</h2>
-                <input
-                  type="text"
-                  id="input-id"
-                  value={email}
-                  onChange={handleEmailChange}
-                  placeholder="이메일을 입력해주세요"
-                />
-                <button
-                  id="id-get-code-btn"
-                  className={!emailValid ? styles.disable : ''}
-                  disabled={!emailValid}
-                >
-                  인증 링크 발송
-                </button>
-              </div>
-            )}
-          </div>
-        </form>
-      </section>
-    </div>
+      <div className={styles['sign-up-form']}>
+        <section className={styles['input-area']}>
+          <form onSubmit={handleSendVerificationLink}>
+            <div className={styles.container}>
+              {verificationSent ? (
+                  <div className={styles['verify-link-sent']}>
+                    <h2>{userType} 회원 등록을 위한 인증 링크가 이메일로 발송되었습니다.</h2>
+                    <p> [ {email} ] </p>
+                    <p>이메일을 확인하여 인증을 완료해주세요.</p>
+                    <button className={styles['resend-login-email-btn']} onClick={onResendEmail}>
+                      이메일을 받지 못하셨나요? 재전송하기
+                    </button>
+                    <button className={styles['retry-sign-up']} onClick={handleRetrySignUp}>
+                      Sign Up other Email address
+                    </button>
+                  </div>
+              ) : (
+                  <div className={styles['id-wrapper']}>
+                    <h2>{userType} 회원 등록을 위한 이메일을 입력해주세요!</h2>
+                    <input
+                        type="text"
+                        id="input-id"
+                        value={email}
+                        onChange={handleEmailChange}
+                        placeholder="이메일을 입력해주세요"
+                    />
+                    <button
+                        id="id-get-code-btn"
+                        className={!emailValid ? styles.disable : ''}
+                        disabled={!emailValid}
+                    >
+                      인증 링크 발송
+                    </button>
+                  </div>
+              )}
+            </div>
+          </form>
+        </section>
+      </div>
   );
 };
 
--- src/pages/auth/SignUpPage.js
+++ src/pages/auth/SignUpPage.js
@@ -1,12 +1,11 @@
-// pages/auth/UserSignUpPage.jsx
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import SignUpForm from '../../components/auth/SignUpForm';
-import styles from './SignUpPage.module.scss'
-import {Link} from "react-router-dom";
+import styles from './SignUpPage.module.scss';
+import { Link } from 'react-router-dom';
 
 const SignUpPage = () => {
-
-  const [userType, setUserType] = useState('customer'); //기본 상태값 customer
+  const [userType, setUserType] = useState('customer'); // 기본 상태값 customer
+  const [verificationSent, setVerificationSent] = useState(false);
 
   // user type 상태관리
   const handleUserTypeChange = (type) => {
@@ -14,44 +13,59 @@ const SignUpPage = () => {
   };
 
   const handleSignUp = (email, password) => {
-    // 개인 회원가입 로직 처리
     console.log('User Sign Up:', email, password);
   };
 
   const handleResendEmail = () => {
-    console.log('이메일 재전송 버튼 누름 !')
+    console.log('이메일 재전송 버튼 누름 !');
   };
 
+  // 이메일 발송 후 사용자 유형 변경 방지
+  const handleVerificationSent = () => {
+    setVerificationSent(true);
+  };
+
+
   return (
-      <>
-        <div className={styles['sign-up-page']}>
-          <div className={styles.container}>
-            <div className={styles['logo']}>FoodieTree</div>
-            <div className={styles['sign-up-section']}>
-              <div className={styles['user-type-buttons']}>
-                <button
-                    className={`${styles['user-type-button']} ${userType === 'customer' ? styles.active : ''}`}
-                    onClick={() => handleUserTypeChange('customer')}
-                >
-                  Customer
-                </button>
-                <button
-                    className={`${styles['user-type-button']} ${userType === 'store' ? styles.active : ''}`}
-                    onClick={() => handleUserTypeChange('store')}
-                >
-                  Store
-                </button>
-              </div>
-              <SignUpForm userType={userType} onSignUp={handleSignUp} onResendEmail={handleResendEmail}/>
-              <div className={'sub-wrapper'}>
-                {/*<a className={'find-email'} href="#"></a>*/}
-                <Link className={styles['sub-login']} to="/login">Login</Link>
-              </div>
+      <div className={styles['sign-up-page']}>
+        <div className={styles.container}>
+          <div className={styles['logo']}>FoodieTree</div>
+          <div className={styles['sign-up-section']}>
+            {!verificationSent && ( // 이메일 발송 후 사용자 유형 버튼 숨기기
+                <div className={styles['user-type-buttons']}>
+                  <button
+                      className={`${styles['user-type-button']} ${
+                          userType === 'customer' ? styles.active : ''
+                      }`}
+                      onClick={() => handleUserTypeChange('customer')}
+                  >
+                    Customer
+                  </button>
+                  <button
+                      className={`${styles['user-type-button']} ${
+                          userType === 'store' ? styles.active : ''
+                      }`}
+                      onClick={() => handleUserTypeChange('store')}
+                  >
+                    Store
+                  </button>
+                </div>
+            )}
+            <SignUpForm
+                userType={userType}
+                onSignUp={handleSignUp}
+                onResendEmail={handleResendEmail}
+                onVerificationSent={handleVerificationSent}
+                verificationSent={verificationSent} // verificationSent 상태 전달
+            />
+            <div className={'sub-wrapper'}>
+              <Link className={styles['sub-login']} to="/login">
+                Login
+              </Link>
             </div>
           </div>
         </div>
-      </>
-
+      </div>
   );
 };
 
