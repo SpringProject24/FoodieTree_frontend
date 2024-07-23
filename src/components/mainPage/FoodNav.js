044aaa034266bfcc82d5c01765f053fd4aa5d9cf
hyerin11 <hodolin24@naver.com>
2024-07-24 01:54:45 +0900

    # DB에서 가게 리스트 불러오기

--- src/components/mainPage/FoodNav.js
+++ src/components/mainPage/FoodNav.js
@@ -1,14 +1,11 @@
-import React, { useState } from "react";
+import React, { useRef, useEffect } from "react";
 import CategoryBtn from "./CategoryBtn";
 import styles from "./FoodNav.module.scss";
-// import bannerImg from "../../assets/images/userMain/header.jpg";
-
-import { useRef, useEffect } from "react";
 import { register } from "swiper/element/bundle";
 
 register();
 
-const FoodNav = ({categories, stores}) => {
+const FoodNav = ({ selectedCategory, stores }) => {
   const swiperElRef = useRef(null);
 
   useEffect(() => {
@@ -22,91 +19,72 @@ const FoodNav = ({categories, stores}) => {
     });
   }, []);
 
-
   return (
     <>
       {/* 내가 찜한 가게 리스트 */}
       <div className={styles.list1}>
         <h2 className={styles.title1}>내가 찜한 가게</h2>
-        <div className={styles["favorite-store-list"]}>
-     
-        </div>
         <swiper-container
-            ref={swiperElRef}
-            slides-per-view="4"
-            navigation="true"
-            pagination="true"
-            loop="true"
-          >
-              {stores.map((store, index) => (
-                <swiper-slide>
-                  <div key={index} className={styles.storeItem}>
-                    <img src={store.image} alt={store.name} />
-                    <p className={styles.storeName}>{store.name}</p>
-                    <span className={styles.storePrice}>{store.price}</span>
-                  </div>
-                </swiper-slide>
-              ))}
-          </swiper-container>
-
+          ref={swiperElRef}
+          slides-per-view="4"
+          navigation="true"
+          pagination="true"
+          loop="true"
+        >
+          {stores.map((store, index) => (
+            <swiper-slide key={index}>
+              <div className={styles.storeItem}>
+                <img src={store.image} alt={store.storeName} />
+                <p className={styles.storeName}>{store.storeName}</p>
+                <span className={styles.storePrice}>{store.price}</span>
+              </div>
+            </swiper-slide>
+          ))}
+        </swiper-container>
       </div>
-
 
       {/* 주변 가게 리스트 */}
       <div className={styles.list1}>
         <h2 className={styles.title1}>주변 가게</h2>
-        <div className={styles["favorite-store-list"]}>
-     
-        </div>
         <swiper-container
-            ref={swiperElRef}
-            slides-per-view="5"
-            navigation="true"
-            pagination="true"
-            loop="true"
-          >
-              {stores.map((store, index) => (
-                <swiper-slide>
-                  <div key={index} className={styles.storeItem}>
-                    <img src={store.image} alt={store.name} />
-                    <p className={styles.storeName}>{store.name}</p>
-                    {/* <span className={styles.discount}>{store.discount}</span> */}
-                    <span className={styles.storePrice}>{store.price}</span>
-                  </div>
-                </swiper-slide>
-              ))}
-          </swiper-container>
-
+          ref={swiperElRef}
+          slides-per-view="5"
+          navigation="true"
+          pagination="true"
+          loop="true"
+        >
+          {stores.map((store, index) => (
+            <swiper-slide key={index}>
+              <div className={styles.storeItem}>
+                <img src={store.image} alt={store.storeName} />
+                <p className={styles.storeName}>{store.storeName}</p>
+                <span className={styles.storePrice}>{store.price}</span>
+              </div>
+            </swiper-slide>
+          ))}
+        </swiper-container>
       </div>
-
-
-
 
       {/* 추천 가게 리스트 */}
       <div className={styles.list1}>
         <h2 className={styles.title1}>추천 가게</h2>
-        <div className={styles["favorite-store-list"]}>
-     
-        </div>
         <swiper-container
-            ref={swiperElRef}
-            slides-per-view="5"
-            navigation="true"
-            pagination="true"
-            loop="true"
-          >
-              {stores.map((store, index) => (
-                <swiper-slide>
-                  <div key={index} className={styles.storeItem}>
-                    <img src={store.image} alt={store.name} />
-                    <p className={styles.storeName}>{store.name}</p>
-                    {/* <span className={styles.discount}>{store.discount}</span> */}
-                    <span className={styles.storePrice}>{store.price}</span>
-                  </div>
-                </swiper-slide>
-              ))}
-          </swiper-container>
-
+          ref={swiperElRef}
+          slides-per-view="5"
+          navigation="true"
+          pagination="true"
+          loop="true"
+        >
+          {stores.map((store, index) => (
+            <swiper-slide key={index}>
+              <div className={styles.storeItem}>
+                <img src={store.image} alt={store.storeName} />
+                <p className={styles.storeName}>{store.storeName}</p>
+                <span className={styles.storePrice}>{store.price}</span>
+              </div>
+            </swiper-slide>
+          ))}
+        </swiper-container>
       </div>
     </>
   );
--- src/pages/userMain/MainPage.js
+++ src/pages/userMain/MainPage.js
@@ -1,30 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import FoodNav from '../../components/mainPage/FoodNav';
-
-import img1 from '../../assets/images/userMain/image1.jpg';
-import img2 from '../../assets/images/userMain/image2.jpg';
-import img3 from '../../assets/images/userMain/image3.jpg';
 import CategoryBtn from '../../components/mainPage/CategoryBtn';
 
 const MainPage = () => {
   const [selectedCategory, setSelectedCategory] = useState('전체');
+  const [stores, setStores] = useState([]);
   const categories = ["한식", "중식", "양식", "일식", "디저트", "카페", "기타"];
 
   const handleCategoryClick = (category) => {
     setSelectedCategory(category);
   };
 
-  // 내가 찜한 가게 리스트 더미
-  const stores = [
-    { name: '공차', category: '카페', image: img1, price: 3900, discount: '55%'},
-    { name: '김밥천국', category: '한식', image: img2, price: 3900  },
-    { name: '메가커피', category: '카페', image: img3, price: 3900  },
-    { name: 'Store 4', category: '양식', image: img1, price: 3900  },
-    { name: 'Store 5', category: '중식', image: img2, price: 3900  },
-    { name: 'Store 6', category: '일식', image: img3, price: 3900  },
-    { name: 'Store 7', category: '양식', image: img3, price: 6900  },
-
-  ];
+  useEffect(() => {
+    // API로부터 데이터 가져오기
+    fetch('http://localhost:8083/storeLists')
+      .then(response => response.json())
+      .then(data => setStores(data))
+      .catch(error => console.error('데이터를 가져오는 중 오류 발생:', error));
+  }, []);
 
   return (
     <div className="main-page">
