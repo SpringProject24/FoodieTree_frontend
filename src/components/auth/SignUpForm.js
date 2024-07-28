cb618b27c8c32d89f0880d4cd879b874e06ccd89
Maybaba <sinyunjong@gmail.com>
2024-07-29 02:35:05 +0900

    # sign in / sign up UXUI edited

--- src/components/auth/SignUpForm.js
+++ src/components/auth/SignUpForm.js
@@ -3,14 +3,32 @@ import React, { useState } from 'react';
 import styles from './SignUpForm.module.scss';
 import commonStyles from '../../common.module.scss';
 import _ from 'lodash';
+import {useNavigate} from "react-router-dom";
 
 
-const SignUpForm = ({ userType, onSignUp, onResendEmail, onVerificationSent }) => {
+const SignUpForm = ({ userType, onVerificationSent }) => {
   const [email, setEmail] = useState('');
   const [emailValid, setEmailValid] = useState(false);
   const [verificationSent, setVerificationSent] = useState(false);
-    const [isExistingUser, setIsExistingUser] = useState(false);
+  const navigate = useNavigate();
+  const [isExistingUser, setIsExistingUser] = useState(false);
+  const [isSending, setIsSending] = useState(false);
 
+    // local storage 에서 토큰 파싱
+    const getTokenFromLocalStorage = () => {
+        console.log(localStorage.getItem('token'))
+        return localStorage.getItem('token');
+    };
+
+    const handleSignInRedirect = () => {
+        const token = getTokenFromLocalStorage();
+        if (token) {
+            console.log("token");
+            navigate(`/verifyEmail?token=${encodeURIComponent(token)}`);
+        } else {
+            console.error('No token found in localStorage');
+        }
+    };
 
   const checkEmailInput = (email) => {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -101,7 +119,10 @@ const sendVerificationLinkForSignUp = async (email) => {
   const handleEmailChange = (e) => {
     const email = e.target.value;
     setEmail(email);
-    if (checkEmailInput(email)) {
+    if (email.trim() === '') {
+        setEmailValid(false);
+        setIsExistingUser(false);
+    } else if (checkEmailInput(email)) {
       debouncedCheckDupId(email);
     } else {
       setEmailValid(false);
@@ -112,24 +133,26 @@ const sendVerificationLinkForSignUp = async (email) => {
     console.log(`Checking duplication for: ${email}`);
     const isUnique = await checkDupId(email);
     setEmailValid(isUnique);
-  }, 100); // 로딩바 구현?
+  }, 500);
 
   const handleSendVerificationLink = async (e) => {
     e.preventDefault();
+    setIsSending(true);
     const isUnique = await checkDupId(email);
     if (!isUnique) {
       alert('입력하신 이메일은 이미 회원입니다.');
+      setIsSending(false);
       return;
     }
     if (isUnique) {
-
       const result = await sendVerificationLinkForSignUp(email);
-
       if (result) {
         setVerificationSent(true);
         onVerificationSent();
+        setIsSending(false);
       } else {
         alert('이메일 전송에 실패했거나 관련된 문제입니다.');
+        setIsSending(false);
       }
     }
   };
@@ -139,6 +162,7 @@ const handleRetrySignUp = () => {
   setVerificationSent(false);
   setEmail('');
   setEmailValid(false);
+  setIsExistingUser(false);
 };
 
   return (
@@ -151,8 +175,10 @@ const handleRetrySignUp = () => {
                     <h2>{userType} 회원 등록을 위한 인증 링크가 이메일로 발송되었습니다.</h2>
                     <p> [ {email} ] </p>
                     <p>이메일을 확인하여 인증을 완료해주세요.</p>
-                    <button className={styles['resend-signup-email-btn']} onClick={handleSendVerificationLink}>
-                      이메일을 받지 못하셨나요? 재전송하기
+                    <button className={isSending ? styles.disable : styles['resend-signup-email-btn']}
+                            onClick={handleSendVerificationLink}>
+
+                        {isSending ? "이메일 전송중..." :"이메일을 받지 못하셨나요? 재전송하기"}
                     </button>
                     <button className={styles['retry-sign-up']} onClick={handleRetrySignUp}>
                       다른 이메일 주소로 회원가입
@@ -160,33 +186,38 @@ const handleRetrySignUp = () => {
                   </div>
               ) : (
                   <div className={styles['id-wrapper']}>
-                    <h2>{userType} 회원 등록을 위한 이메일을 입력해주세요!
-                        {isExistingUser ? (
-                            <>
-                                <br/><br/>
-                                이미 존재하는 이메일 계정입니다.
-                            </>
-                        ) : (
-                            <>
-                                <br /><br/>
-                                환영해요!
-                            </>
-                            )}
-                    </h2>
+                      <h2>{userType} 회원 등록을 위한 이메일을 입력해주세요!
+                          {isExistingUser ? (
+                              <>
+                                  <br/><br/>
+                                  이미 존재하는 이메일 계정입니다.
+                              </>
+                          ) : email.trim() === '' ? (
+                              <>
+                                  <br/><br/>
+                              </>
+                          ) : (
+                              <>
+                                  <br/><br/>
+                                  푸디트리에 와주셔서 환영합니다!
+                              </>
+                          )}
+                      </h2>
                       <input
                           type="text"
-                        id="input-id"
-                        value={email}
-                        onChange={handleEmailChange}
-                        placeholder="이메일을 입력해주세요"
-                    />
-                    <button
-                        id="id-get-code-btn"
-                        className={!emailValid ? styles.disable : ''}
-                        disabled={!emailValid}
-                    >
-                      회원가입 인증메일 발송
-                    </button>
+                          id="input-id"
+                          value={email}
+                          onChange={handleEmailChange}
+                          placeholder="이메일을 입력해주세요"
+                      />
+                      <button
+                          id="id-get-code-btn"
+                          className={(!emailValid ? styles.disable : '') || (isSending ? styles.disable : '')}
+                          disabled={isExistingUser||!emailValid}
+                          onClick={(!isExistingUser && !isSending) ? handleSignInRedirect : null}
+                      >
+                          {isSending ? "이메일 전송중..." : (!isExistingUser ? '회원가입' : '회원가입 인증메일 발송')}
+                      </button>
                   </div>
               )}
             </div>
