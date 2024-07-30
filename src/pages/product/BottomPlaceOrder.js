14243802be681bf13fe6b447c150ac29f3d4a468
hyerin11 <hodolin24@naver.com>
2024-07-30 15:00:46 +0900

    # BottomPlaceOrder수정

--- src/pages/product/BottomPlaceOrder.js
+++ src/pages/product/BottomPlaceOrder.js
@@ -1,15 +1,41 @@
 import React from 'react';
-import styles from './BottomPlaceOrder.module.scss';
+import styles from "./BottomPlaceOrder.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMinus, faPlus } from "@fortawesome/free-solid-svg-icons";
 
-const BottomPlaceOrder = ({ makeReservation, initialCount, handleIncrease, handleDecrease }) => {
-    if (!initialCount) return null;
+const BottomPlaceOrder = ({ makeReservation, productDetail, initialCount, handleIncrease, handleDecrease }) => {
+
+    // productDetail의 존재를 확인하고, 없으면 기본 값을 사용
+    const remainingProduct = productDetail?.storeInfo?.remainProduct || Infinity;
+
+    // 구매하기 버튼 클릭 핸들러
+    const handleMakeReservation = () => {
+        console.log(initialCount);
+        makeReservation(initialCount);
+    }
 
     return (
         <div className={styles.bottomPlaceOrder}>
-            <button onClick={handleDecrease}>-</button>
-            <span>{initialCount}</span>
-            <button onClick={handleIncrease}>+</button>
-            <button onClick={() => makeReservation(initialCount)}>주문하기</button>
+            <div className={styles.productAmtAdjustBtn}>
+                <button 
+                    className={styles.adjustBtn} 
+                    onClick={handleDecrease} 
+                    disabled={initialCount <= 1}
+                >
+                    <FontAwesomeIcon icon={faMinus} />
+                </button>
+                <p className={styles.initialCnt}>{initialCount}</p>
+                <button 
+                    className={styles.adjustBtn} 
+                    onClick={handleIncrease} 
+                    disabled={initialCount >= remainingProduct}
+                >
+                    <FontAwesomeIcon icon={faPlus} />
+                </button>
+            </div>
+            <div className={styles.placeOrderBtn} onClick={handleMakeReservation}>
+                <p>구매하기</p>
+            </div>
         </div>
     );
 };
