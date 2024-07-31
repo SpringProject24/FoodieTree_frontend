1c1c5e1b3bce3430f7d297c4d65911492c18df6f
Maybaba <sinyunjong@gmail.com>
2024-07-31 23:39:33 +0900

    auto access token test

--- src/components/auth/VerifyToken.js
+++ src/components/auth/VerifyToken.js
@@ -31,6 +31,7 @@ function VerifyToken() {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + token,
           },
           body: JSON.stringify({ token: tokenToVerify }),
         });
