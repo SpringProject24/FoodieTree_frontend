45d3bdf55e78c62f5b85d59cc7a3204112c6c117
dazzlingtea <kitsnr08@gmail.com>
2024-08-07 20:28:51 +0900

    WIP add checkbox

--- src/components/admin/approval/ApprovalTable.js
+++ src/components/admin/approval/ApprovalTable.js
@@ -2,25 +2,47 @@ import React, {useState} from 'react';
 import {
   flexRender,
   getCoreRowModel,
-  getFilteredRowModel,
+  getFilteredRowModel, getPaginationRowModel,
   getSortedRowModel,
   useReactTable
 } from "@tanstack/react-table";
 import SearchInTable from "./SearchInTable";
 import {BiSortAlt2} from "react-icons/bi";
+import styles from "./ApprovalTables.module.scss";
 
 const columns = [
+  {
+    id: 'select',
+    header: ({ table }) => (
+      <input
+        id="header-checkbox"
+        type="checkbox"
+        checked={table.getIsAllPageRowsSelected()} // 전체 row가 선택되었는지 확인
+        onChange={table.getToggleAllPageRowsSelectedHandler()} // 전체 row를 선택/해제하는 handler
+      />
+    ),
+    cell: ({ row }) => (
+      <input
+        id={`cell-checkbox-${row.id}`}
+        type="checkbox"
+        checked={row.getIsSelected()} // row가 선택되었는지 확인
+        disabled={!row.getCanSelect()} // row가 선택 가능한지 확인
+        onChange={row.getToggleSelectedHandler()} // row를 선택/해제하는 handler
+      />
+    ),
+    size: 50,
+  },
   {accessorKey: 'name', header: '상호명', cell: (props) => <p>{props.getValue()}</p>},
   {accessorKey: 'storeId', header: '스토어 계정', cell: (props) => <p>{props.getValue()}</p>},
-  {accessorKey: 'status', header: '상태', cell: (props) => <p>{props.getValue()}</p>},
-  {accessorKey: 'createdAt', header: '생성일시', cell: (props) => <p>{props.getValue().toLocaleString()}</p>},
-  {accessorKey: 'license', header: '사업자등록번호', cell: (props) => <p>{props.getValue()}</p>},
-  {accessorKey: 'licenseVerification', header: '유효성검사', cell: (props) => <p>{props.getValue()}</p>},
-  {accessorKey: 'category', header: '업종', cell: (props) => <p>{props.getValue()}</p>},
+  {accessorKey: 'status', header: '상태', size: '100', cell: (props) => <p>{props.getValue()}</p>},
+  {accessorKey: 'createdAt', header: '생성일시', size: '200', cell: (props) => <p>{props.getValue().toLocaleString()}</p>},
+  {accessorKey: 'license', header: '사업자등록번호', size: '130', cell: (props) => <p>{props.getValue()}</p>},
+  {accessorKey: 'licenseVerification', header: '유효성', size: '100', cell: (props) => <p>{props.getValue()}</p>},
+  {accessorKey: 'category', header: '업종', size: '100', cell: (props) => <p>{props.getValue()}</p>},
   {accessorKey: 'contact', header: '연락처', cell: (props) => <p>{props.getValue()}</p>},
   {accessorKey: 'address', header: '주소', cell: (props) => <p>{props.getValue()}</p>},
-  {accessorKey: 'productCnt', header: '상품 수량', cell: (props) => <p>{props.getValue()}</p>},
-  {accessorKey: 'price', header: '상품 가격', cell: (props) => <p>{props.getValue()}</p>},
+  {accessorKey: 'productCnt', header: '수량', size: '50', cell: (props) => <p>{props.getValue()}</p>},
+  {accessorKey: 'price', header: '가격', size: '80', cell: (props) => <p>{props.getValue()}</p>},
   {accessorKey: 'proImage', header: '상품 구성 예시', cell: (props) => <p>{props.getValue()}</p>},
 
 ];
@@ -75,6 +97,7 @@ const DATA = [
 const ApprovalTable = () => {
   const [data, setData] = useState(DATA);
   const [columnFilters, setColumnFilters] = useState([]);
+  const [rowSelection, setRowSelection] = useState({});
 
   const table = useReactTable({
     data,
@@ -82,21 +105,29 @@ const ApprovalTable = () => {
     getCoreRowModel: getCoreRowModel(),
     getFilteredRowModel: getFilteredRowModel(),
     getSortedRowModel: getSortedRowModel(),
+    onRowSelectionChange: setRowSelection,
+    getPaginationRowModel: getPaginationRowModel(),
     state: {
-      columnFilters
+      columnFilters,
+      rowSelection,
     },
   });
+  console.log("헤더 확인: ", table.getHeaderGroups())
+  console.log("row 확인: ", rowSelection)
 
   return (
     <>
       <SearchInTable columnFilters={columnFilters} setColumnFilters={setColumnFilters} />
-      <table width={table.getTotalSize()}>
+      <table width={table.getTotalSize()} className={styles['tbl-line']}>
         <thead>
         {table.getHeaderGroups().map(headerGroup =>
           <tr key={headerGroup.id}>
             {headerGroup.headers.map(header =>
               <th key={header.id} width={header.getSize()}>
-                {header.column.columnDef.header}
+                {flexRender(
+                  header.column.columnDef.header,
+                  header.getContext(),
+                )}
                 {
                   header.column.getCanSort() &&
                   <BiSortAlt2
@@ -126,6 +157,10 @@ const ApprovalTable = () => {
 
         </tbody>
       </table>
+        <div>
+          {table.getState().pagination.pageIndex + 1 /*현재 페이지*/}
+          {table.getPageCount() /*총 페이지 수*/}
+        </div>
     </>
   );
 };
