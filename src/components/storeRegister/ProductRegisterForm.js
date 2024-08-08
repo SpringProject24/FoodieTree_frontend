40b1f11bee5b2bd3d64e19a8997f4e15bb27663d
hoseong511 <thdghtjd115@naver.com>
2024-08-08 22:50:27 +0900

    added token store

--- src/components/storeRegister/ProductRegisterForm.js
+++ src/components/storeRegister/ProductRegisterForm.js
@@ -6,6 +6,7 @@ import PriceRadioBox from "./PriceRadioBox";
 import {STORE_URL} from "../../config/host-config";
 import useFormValidation from "./useFormValidation";
 import ErrorSpan from "./ErrorSpan";
+import {authFetch} from "../../utils/authUtil";
 
 // 상품 가격 옵션 배열
 const PRICE_OPTIONS = [
@@ -71,7 +72,7 @@ const ProductRegisterForm = () => {
 
     console.log('payload 이미지 확인: ', payload.get('productImage'))
 
-    const response = await fetch(`${STORE_URL}/approval/p`, {
+    const response = await authFetch(`${STORE_URL}/approval/p`, {
       method: 'POST',
       headers: {
         // 'Content-Type': 'multipart/form-data', FormData 생략 가능
