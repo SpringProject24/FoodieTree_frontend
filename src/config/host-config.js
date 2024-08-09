efda9dd620c65094292c03db31cb2bfd10f622df
dazzlingtea <kitsnr08@gmail.com>
2024-08-09 18:27:52 +0900

    WIP add admin url

--- src/config/host-config.js
+++ src/config/host-config.js
@@ -2,9 +2,11 @@ const STORE = '/store';
 const CUSTOMER = '/customer';
 const STORELISTS = '/storeLists';
 const EMAIL = '/email';
+const ADMIN = '/admin';
 
 export const STORE_URL =  STORE;
 export const CUSTOMER_URL = CUSTOMER;
 export const STORELISTS_URL = STORELISTS;
 
 export const EMAIL_URL = EMAIL;
+export const ADMIN_URL = ADMIN;
