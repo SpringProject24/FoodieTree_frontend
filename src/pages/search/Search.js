7418dc8312482d5ca29ea811563ab3e2f7e1dab5
hoseong511 <thdghtjd115@naver.com>
2024-08-15 10:18:26 +0900

    WIP 무한스크롤

--- src/pages/search/Search.js
+++ src/pages/search/Search.js
@@ -7,22 +7,28 @@ const Search = () => {
     const [word, setWord] = useSearchParams();
     const [pageNo, setPageNo] = useState(1);
     const [storeList, setStoreList] = useState([]);
+
     useEffect(() => {
         (async () => {
-            const payload = {
-                pageNo,
-                keyword: word.get('q')
-            }
-            const response = await fetch(`/search?pageNo=${pageNo}&keyword=${word.get('q')}`);
-            const data = await response.json();
-            if (response.ok) {
-                console.log(data);
-                setStoreList(data.result);
-            } else {
-                alert("잠시 후 다시 이용해주세요.");
-            }
+            await fetchSearch();
         })();
     }, [word]);
+
+    useEffect(() => {
+
+    }, []);
+
+    const fetchSearch = async () => {
+        const response = await fetch(`/search?pageNo=${pageNo}&keyword=${word.get('q')}`);
+        const data = await response.json();
+        if (response.ok) {
+            console.log(data);
+            setStoreList(data.result);
+        } else {
+            alert("잠시 후 다시 이용해주세요.");
+        }
+    }
+
     return (
         <div>
             <SearchList stores={storeList}/>
