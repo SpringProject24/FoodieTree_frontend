bcd7e7ef8d31f1e14d517ef2159bd53afccc5c5b
Jeong-in <162551171+munginisfree@users.noreply.github.com>
2024-08-17 01:13:46 +0900

    navigate to customer issue page

---
+++ src/pages/customer/CustomerIssuePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CustomerIssuePage = () => {
+    return (
+        <div>
+            <h1>CustomerIssuePage</h1>
+        </div>
+    );
+};
+
+export default CustomerIssuePage;
