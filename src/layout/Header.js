b95c925e81d95a3b627800924a7b6708bcd3040f
Maybaba <sinyunjong@gmail.com>
2024-08-16 12:48:20 +0900

    # header design - edit SideBarBtn.module.scss position top 18px to 13px - header display flex 적용

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,11 +1,8 @@
 import React, { useEffect, useState } from 'react';
-
-import LogoutLoginBtn from "../components/header/LogoutLoginBtn";
-
 import { useNavigate } from 'react-router-dom';
-import {getRefreshToken, getToken, logoutAction} from '../utils/authUtil';
+import { getToken, getRefreshToken } from '../utils/authUtil';
 import styles from './Header.module.scss';
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import SideBarBtn from "../components/store/mypage-edit/SideBarBtn";
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
@@ -21,74 +18,56 @@ const Header = () => {
     const [width, setWidth] = useState(window.innerWidth);
 
     useEffect(() => {
-        if(getToken() && getRefreshToken()) {
+        if (getToken() && getRefreshToken()) {
             setIsAuthenticated(true);
-        } else setIsAuthenticated(false);
+        } else {
+            setIsAuthenticated(false);
+        }
     }, []);
 
     const showHandler = () => {
         setShow(prev => !prev);
     }
 
-
     return (
-        <header>
-            {/*<LogoutLoginBtn />*/}
+        <header className={styles.header}>
+            {/* 햄버거 버튼 */}
+            {width > 400 && <SideBarBtn  className={styles.sideBarBtn} onShow={showHandler} />}
 
-            {/*햄버거 버튼*/}
-            {width > 400 && <SideBarBtn onShow={showHandler}/>}
-
-            {/*로고*/}
+            {/* 로고 */}
             <div className={styles.logoBtn}></div>
 
-            {/*상점검색칸*/}
+            {/* 현재 위치 */}
+            <div className={styles.locationPinIcon}></div>
+            <div className={styles.areaName}>
+                현재 위치를 확인할 수 없어요.
+            </div>
+            <div className={styles.dot}>・︎</div>
+            <div className={styles.selectedAreaCategory}> Now </div>
+            <button className={styles.selectedAreaCategoryBtn}></button>
+
+            {/* 상점 검색 칸 */}
             <form className={styles.searchStoreSection}>
                 <button className={styles.magnifyClickBtn}>
-                    <FontAwesomeIcon icon="fas fa-search" className={styles.magnifyIcon}/>
+                    <FontAwesomeIcon icon="fa-solid fa-search" className={styles.magnifyIcon} />
                 </button>
                 <input
                     type="text"
-                    id="input-keyword-id"
-                    // value={keyword}
-                    // onChange={handleEmailChange}
                     placeholder="여기에 음식점 혹은 위치를 검색해보세요."
                 />
             </form>
 
-            {/*현재 위치*/}
-            <div className={styles.locationPinIcon}></div>
-            <div className={styles.areaName}
-                 // defaultValue={}
-            >
-                현재 위치를 확인할 수 없어요.
-            </div>
-            <div className={styles.selectedAreaCategory}> Now </div>
-            <button className={styles.selectedAreaCategoryBtn}></button>
-
-            {/*로그아웃 상태 시 로그인 및 회원가입 버튼 */}
+            {/* 로그인 및 회원가입 버튼 */}
             {isAuthenticated ?
                 <MyInfo /> :
                 <div className={styles.loginBtnSection}>
-                <button className={styles.signInBtn}  onClick={() => navigate('/sign-in')}> Sign in </button>
-                <button className={styles.signUpBtn}  onClick={() => navigate('/sign-up')}> Sign up </button>
+                    <button className={styles.signInBtn} onClick={() => navigate('/sign-in')}> Sign in</button>
+                    <div className={styles.dot}>・︎</div>
+                    <button className={styles.signUpBtn} onClick={() => navigate('/sign-up')}> Sign up</button>
                 </div>
             }
-
-
-            {/*로그아웃 버튼*/}
-            {/*{isAuthenticated ? (*/}
-            {/*    <button onClick={handleLogout} className={styles.logoutBtn}>*/}
-            {/*        Log Out*/}
-            {/*    </button>*/}
-            {/*) : (*/}
-            {/*    <button onClick={() => navigate('/sign-in')} className={styles.loginBtn}>*/}
-            {/*        Log In*/}
-            {/*    </button>*/}
-            {/*)}*/}
-
-
         </header>
-
     );
 }
+
 export default Header;
