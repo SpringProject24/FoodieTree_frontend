b41104b818ac6ca87d2230f910f5ae1e29834e2f
Maybaba <sinyunjong@gmail.com>
2024-08-17 01:33:19 +0900

    #footer design done

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -12,26 +12,46 @@ const images = [food1, food2, food3, imagination1, imagination2, nature1, nature
 
 const Footer = () => {
     const [currentImageIndex, setCurrentImageIndex] = useState(0);
+    const [isFading, setIsFading] = useState(false);
+    const [copySuccess, setCopySuccess] = useState(false);
+
 
     useEffect(() => {
         const intervalId = setInterval(() => {
-            setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
-        }, 3000); // 3초마다 이미지 전환
+            setIsFading(true);
+            setTimeout(() => {
+                setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+                setIsFading(false);
+            }, 1000); // 1초 후 이미지 전환
+        }, 4000); // 4초마다 이미지 전환
 
-        return () => clearInterval(intervalId); // 컴포넌트 언마운트 시 interval 정리
+        return () => clearInterval(intervalId);
     }, []);
+
+    // 이메일 클립보드 복사
+    const handleEmailClick = () => {
+        navigator.clipboard.writeText("foodie.treee@gmail.com")
+            .then(() => {
+                setCopySuccess(true);
+                setTimeout(() => setCopySuccess(false), 2000); // 2초 후 복사 성공 메시지 초기화
+            })
+            .catch(err => console.error('Failed to copy text: ', err));
+    };
 
     return (
         <footer className={styles.footer}>
             <div className={styles.pictureDiv}>
-                <img
-                    src={images[currentImageIndex]}
-                    alt="Footer Image"
-                    className={styles.footerImage}
-                />
+                    <img
+                        src={images[currentImageIndex]}
+                        alt="Footer Image"
+                        className={`${styles.footerImage} ${isFading ? styles.fadeOut : styles.fadeIn}`}
+                    />
             </div>
             <span className={styles.emailTitle}>Based on Republic of Korea,</span>
-            <span className={styles.email}>foodie.treee@gmail.com</span>
+            <span className={styles.email} onClick={handleEmailClick}>
+                foodie.treee@gmail.com
+            </span>
+            {copySuccess && <span className={styles.copySuccess}>Copied!</span>}
             <span className={styles.copyRight}>©2024 FoodieTree All Rights Reserved.</span>
 
             <div className={styles.footerLinks}>
