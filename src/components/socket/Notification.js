bb501e66ac49a9e1fc2f728f31527677c4f8877d
dazzlingtea <kitsnr08@gmail.com>
2024-08-18 19:02:29 +0900

    style layout

--- src/components/socket/Notification.js
+++ src/components/socket/Notification.js
@@ -74,11 +74,11 @@ const Notification = ({email, role}) => {
   console.log('토글상태 ', isOpen)
 
   return (
-    <div>
+    <div className={styles['notify-container']}>
       <div className={`${styles['notify-icon']} ${hasNewMessage && styles.new}`} onClick={toggleAlerts}>
         <IoNotificationsOutline />
       </div>
-      <ul className={`${styles['notify-list']} ${isOpen && styles.close}`}>
+      <ul className={`${styles['notify-list']} ${!isOpen && styles.close}`}>
         <li>알림 {notifications.length}건</li>
         {notifications.slice().reverse().map((notification, index) => (
           <li key={index} onClick={() => window.location.href = `/${role}`}>
