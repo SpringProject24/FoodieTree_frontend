d09ce70f513c0379a5f9f3401203462e6a527beb
dazzlingtea <kitsnr08@gmail.com>
2024-08-19 22:27:57 +0900

    add fetch getting list of notifications

--- src/components/notification/Notification.js
+++ src/components/notification/Notification.js
@@ -1,40 +1,54 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import SockJS from 'sockjs-client';
-import { Stomp } from '@stomp/stompjs';
+import {Stomp} from '@stomp/stompjs';
 import styles from './Notification.module.scss';
 import {IoNotificationsOutline} from "react-icons/io5";
+import {authFetch} from "../../utils/authUtil";
 
 const Notification = ({email, role}) => {
   const [stompClient, setStompClient] = useState(null);
   const [notifications, setNotifications] = useState([]);
   const [isOpen, setIsOpen] = useState(false);
   const [hasNewMessage, setHasNewMessage] = useState(false);
+  const BASE_URL = window.location.origin;
+  console.log('BASE_URL 값 ', BASE_URL)
 
   console.log('이메일 ', email, role);
   let customerId = null;
   let storeId = null;
-  if(role === 'customer') {
+  if (role === 'customer') {
     customerId = email;
-  }
-  else {
+  } else {
     storeId = email;
   }
+  // REST API로 알림 목록 fetch
+  useEffect(() => {
+    const fetchNotifications = async () => {
+      try {
+        const response = await authFetch(`/notifications`);
+        if (!response.ok) {
+          console.error('Failed to fetch notifications');
+        }
+        const data = await response.json();
+        setNotifications(prev => [...prev, ...data]);
+      } catch (error) {
+        console.error('알림 목록 fetch Error :', error);
+      }
+    };
+    fetchNotifications();
+  }, []);
 
   useEffect(() => {
     const connectWebSocket = () => {
       const socket = new SockJS('http://localhost:3000/noti');
       const client = Stomp.over(socket);
 
-      // heartbeat 설정
-      // client.heartbeat.outgoing = 20000; // 10초마다 클라이언트 -> 서버로 heartbeat
-      // client.heartbeat.incoming = 20000; // 10초마다 서버 -> 클라이언트로 heartbeat
-
       client.connect({}, () => {
         if (role === 'customer') {
           // 고객 알림 구독
           client.subscribe(`/queue/customer/${customerId}`, (message) => {
             console.log('Received message : ', message);
-            const notification = JSON.parse(message.body); // 메시지가 JSON이라면 파싱 필요
+            const notification = JSON.parse(message.body);
             setNotifications(prev => [...prev, notification]);
             setHasNewMessage(true);
           });
@@ -42,7 +56,7 @@ const Notification = ({email, role}) => {
           // 가게 알림 구독
           client.subscribe(`/topic/store/${storeId}`, (message) => {
             console.log('Received message for store: ', message);
-            const notification = JSON.parse(message.body); // 메시지가 JSON이라면 파싱 필요
+            const notification = JSON.parse(message.body);
             setNotifications(prev => [...prev, notification]);
             setHasNewMessage(true);
           });
@@ -76,11 +90,11 @@ const Notification = ({email, role}) => {
   return (
     <div className={styles['notify-container']}>
       <div className={`${styles['notify-icon']} ${hasNewMessage && styles.new}`} onClick={toggleAlerts}>
-        <IoNotificationsOutline />
+        <IoNotificationsOutline/>
       </div>
       <ul className={`${styles['notify-list']} ${!isOpen && styles.close}`}>
         <li>알림 {notifications.length}건</li>
-        {notifications.slice().reverse().map((notification, index) => (
+        {notifications?.slice().reverse().map((notification, index) => (
           <li key={index} onClick={() => window.location.href = `/${role}`}>
             {notification.content}
           </li>
