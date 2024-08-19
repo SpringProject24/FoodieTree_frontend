1a9072fa45d52bf0bcae0c7c92165d18d6c8b338
hansolkim9 <masicer2@gmail.com>
2024-08-19 11:11:13 +0900

    # CustomerMyPage: 예약상세 모달에서 픽업 확인 삭제(주석)

--- src/pages/customer/CustomerReservationDetailModal.js
+++ src/pages/customer/CustomerReservationDetailModal.js
@@ -104,12 +104,12 @@ const CustomerReservationDetailModal = ({reservationDetail, onPickupConfirm}) =>
                     </div>
                     {tag}
                 </div>
-                {reservationDetail.status === 'RESERVED' ?
-                    <>
-                        <p className={styles.btnDes}>가게에 도착해 스페셜 팩을 수령했다면 <br/> '픽업 확인' 버튼을 눌러주세요!</p>
-                        <button className={styles.pickupConfirmBtn} onClick={handleConfirmPickUp}>픽업 확인</button>
-                    </>
-                    : <></>}
+                {/*{reservationDetail.status === 'RESERVED' ?*/}
+                {/*    <>*/}
+                {/*        <p className={styles.btnDes}>가게에 도착해 스페셜 팩을 수령했다면 <br/> '픽업 확인' 버튼을 눌러주세요!</p>*/}
+                {/*        <button className={styles.pickupConfirmBtn} onClick={handleConfirmPickUp}>픽업 확인</button>*/}
+                {/*    </>*/}
+                {/*    : <></>}*/}
 
                 <div className={styles.needHelp}>도움이 필요하신가요?</div>
                 {/* 추후 추가될 기능 문의하기 버튼 */}
